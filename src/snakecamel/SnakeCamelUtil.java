6e25cf192fd4ea87dc9e524e5669e5b4c4d77dca
stardust224 <kerogeros@icloud.com>
2018-05-17 12:22:56 +0900

    capitalize(),uncapitalize(),snakeToCamelCase(),camelToSnakeCase()を変更しました。

--- src/snakecamel/SnakeCamelUtil.java
+++ src/snakecamel/SnakeCamelUtil.java
@@ -2,45 +2,46 @@ package snakecamel;
 
 public class SnakeCamelUtil {
 
-	public static String snakeToCamelcase(String snake_case) {
-		String[] words = snake_case.split("_");
+	public static String snakeToCamelcase(String snake_case) {//スネークをキャメルにする
+		String[] words = snake_case.split("_",-1);//"_"で分割された単語をwords配列へ入れる
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
-			sb.append(capitalize(words[i]));
+		for (int i = 0; i < words.length; i++) {
+			sb.append(capitalize(words[i]));//分割した単語同士をつなげる。単語の頭は大文字。
 		}
 		return new String(sb);
 	}
 	 
-	public static String camelToSnakecase(String camelcase) {
+	public static String camelToSnakecase(String camelcase) {//キャメルをスネークにする
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
 		for (int i = 0; i < camelcase.length(); i++) {
 			char c = camelcase.charAt(i);
-			if (Character.isUpperCase(c)) {
-				sb.append(camelcase.substring(j, i));
+			if (Character.isUpperCase(c)) {//cが大文字ならば
+				sb.append(camelcase.substring(j, i));//
 				if (sb.length() > 0) {
 					sb.append("_");
 				}
-				sb.append(Character.toLowerCase(c));
-				j = i;
+				sb.append(Character.toLowerCase(c));//cを小文字にする
+				j = i+1;
 			}
 		}
 		sb.append(camelcase.substring(j));
 		return new String(sb);
 	}
 	
-	static String capitalize(String s) {
+	static String capitalize(String s) {//1文字目を大文字にする
+		if(s.length()==0)return "";
 		char first = s.charAt(0);
-		char upperFirst = Character.toUpperCase(first);
-		String rest = s.substring(1);
-		return upperFirst + rest;
+		char upperFirst = Character.toUpperCase(first);//大文字へ変換
+		String rest = s.substring(1);//2文字目からrestに入れる
+		return upperFirst + rest;//1文字目を大文字にした文字列を返す
 	}
 
-	static String uncapitalize(String s) {
+	static String uncapitalize(String s) {//1文字目を小文字にする
 		char first = s.charAt(0);
-		char lowerFirst = Character.toLowerCase(first);
+		char lowerFirst = Character.toLowerCase(first);//小文字へ変換
 		String rest = s.substring(1);
-		return lowerFirst + rest;
+		return lowerFirst + rest;//1文字目を小文字にした文字列を返す
 	}
 	
 }
---
+++ test/snakecamel/SnakeCamelUtilTest.java
@@ -0,0 +1,108 @@
+package snakecamel;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+
+public class SnakeCamelUtilTest {
+
+	@Test
+	public void 空文字から空文字1() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = " ";
+		String actual = calc.capitalize(" ");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void aからA() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "A";
+		String actual = calc.capitalize("a");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void xyzからXyz() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "Xyz";
+		String actual = calc.capitalize("xyz");
+		assertThat(actual,is(expected));
+	}
+	public void 空文字から空文字2() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = " ";
+		String actual = calc.uncapitalize(" ");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void Aからa() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "xyz";
+		String actual = calc.uncapitalize("Xyz");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void Xyzからxyz() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "xyz";
+		String actual = calc.uncapitalize("Xyz");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void abc_defからAbcDef() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "AbcDef";
+		String actual = calc.snakeToCamelcase("abc_def");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void abcからAbc() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "Abc";
+		String actual = calc.snakeToCamelcase("abc");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void abc_def_ghからAbcDefGh() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "AbcDefGh";
+		String actual = calc.snakeToCamelcase("abc_def_gh");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void abc__def___ghからAbcDefGh() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "AbcDefGh";
+		String actual = calc.snakeToCamelcase("abc__def___gh");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void _abc_def__からAbcDef() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "AbcDef";
+		String actual = calc.snakeToCamelcase("_abc_def__");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void AbcDefからabc_def() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "abc_def";
+		String actual = calc.camelToSnakecase("AbcDef");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void Abcからabc() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "abc";
+		String actual = calc.camelToSnakecase("Abc");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void AbcDefGhからabc_def_gh() {
+		SnakeCamelUtil calc = new SnakeCamelUtil();
+		String expected = "abc_def_gh";
+		String actual = calc.camelToSnakecase("AbcDefGh");
+		assertThat(actual,is(expected));
+	}
+}
